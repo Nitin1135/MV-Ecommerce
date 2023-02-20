94a8f7c2d5fae3ac321725c32155373932a6b5eb
Nitin Patel <nitin4532@gmail.com>
2023-02-20 16:48:03 +0530

    cart page create

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -77,3 +77,62 @@ $('.trends_season').owlCarousel({
     }
 })
 
+// product cart plus minus 
+// $(document).ready(function() {
+    $('.minus').click(function () {
+      var $input = $(this).parent().find('input');
+      var count = parseInt($input.val()) - 1;
+      count = count < 1 ? 1 : count;
+      $input.val(count);
+      $input.change();
+      return false;
+    });
+    $('.plus').click(function () {
+      var $input = $(this).parent().find('input');
+      $input.val(parseInt($input.val()) + 1);
+      $input.change();
+      return false;
+    });
+//   });
+
+
+
+// products view slide zoom 
+
+var galleryThumbs = new Swiper('.gallery-thumbs', {
+    spaceBetween: 10,
+    freeMode: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+    breakpoints: {
+        0: {
+            slidesPerView: 3,
+        },
+        992: {
+            slidesPerView: 8,
+        },
+    }
+});
+var galleryTop = new Swiper('.gallery-top', {
+    spaceBetween: 10,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    thumbs: {
+        swiper: galleryThumbs
+    },
+});
+// change carousel item height
+// gallery-top
+let productCarouselTopWidth = $('.gallery-top').outerWidth();
+$('.gallery-top').css('height', productCarouselTopWidth);
+
+// gallery-thumbs
+let productCarouselThumbsItemWith = $('.gallery-thumbs .swiper-slide').outerWidth();
+$('.gallery-thumbs').css('height', productCarouselThumbsItemWith);
+
+// activation zoom plugin
+var $easyzoom = $('.easyzoom').easyZoom();
+
+
